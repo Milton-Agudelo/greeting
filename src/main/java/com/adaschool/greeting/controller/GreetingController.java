740866b5ce6d6c70f4c03db85777f75fef7544a8
Milton Agudelo <Milton_Agudelo@yahoo.com>
2022-09-12 23:10:10 -0500

    procfile file added.

--- src/main/java/com/adaschool/greeting/controller/GreetingController.java
+++ src/main/java/com/adaschool/greeting/controller/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/greeting/")
+@RequestMapping
 public class GreetingController {
 
     private final IGreetingService iGreetingService;
